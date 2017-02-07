fd568c5261f03762c412a4e7ee86a15bfaa9e9e3
Alex Nesterov <ialexnesterov@gmail.com>
2017-02-07 03:38:09 +0300

    Add preloader

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,22 @@
 $(function(){
   console.log('OK!');
+
+// Preloader
+$(function () {
+  var page = $('.page');
+  var preloader = $('.preloader');
+  var item = preloader.find('.preloader__status');
+
+  function hidePreloader() {
+    item.fadeOut();
+    setTimeout(function () {
+      preloader.fadeOut('slow', function() {
+        page.css({'overflow':'visible'});
+        preloader.remove();
+      });
+    }, 300);
+  }
+  // hidePreloader();
+});
+
 });
