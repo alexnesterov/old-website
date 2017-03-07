5376341684ef473899c50577d4d785efa0a582de
Alex Nesterov <ialexnesterov@gmail.com>
2017-03-07 13:22:32 +0300

    Update home page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,8 +18,36 @@ $(function () {
   }
   $(window).on('load', function() {
     hidePreloader();
-    $('.profile__info').addClass('animate');
   });
 });
+
+// Info block height automate
+// $(function () {
+//   var infoBlock = $('.info');
+//   var infoContainer = $('.info .container');
+//   var containerHeight = infoContainer.height();
+//   var infoBlockHeight = infoBlock.height();
+//
+//   var headerHeight = $('.header').height();
+//   var footerHeight = $('.footer').height();
+//
+//   infoBlock.css({
+//     paddingTop: headerHeight,
+//     paddingBottom: footerHeight
+//   });
+//
+//   var setStatic = function () {
+//     infoBlock.removeClass('_static');
+//     containerHeight = infoContainer.height();
+//     infoBlockHeight = infoBlock.height();
+//     if (infoBlockHeight < containerHeight) {
+//       infoBlock.addClass('_static');
+//     }
+//   };
+//   setStatic();
+//   $(window).resize(function() {
+//     setStatic();
+//   });
+// });
 
 });
